5c83aae90c0583aefba7074c0214a5effb308abc
soumyamainknandi <soumyanandi22@gmail.com>
2025-06-09 20:47:05 +0530

    Fix overlapping issue in login/signup form

--- src/Pages/LoginSign.jsx
+++ src/Pages/LoginSign.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import './CSS/Login.css'
+
 const LoginSign = () => {
   return (
     <div className='loginsignup'>
       <div className='loginsignup-container'>
         <h1>Sign Up</h1>
         <div className='loginsignup-fields'>
-          <input type="text" placeholder='Your Name'/>
-          <input type="email" placeholder='Email Address'/>
-          <input type="password" placeholder='Password'/>
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
         </div>
-        <button className=''>Continue</button>
-        <p className='loginsignup-login'
->Already have an account?
-<span>Login</span>
-<div className='loginsignup-agree'>
-  <input type="checkbox" name="" id=""/>
-  <p>By continuing ,i agree to the trems of use & privacy policy.</p>
-</div>
-</p>      
-</div>
-      
+
+        <button>Continue</button>
+
+        {/* Correctly placed below the button */}
+        <p className='loginsignup-login'>
+          Already have an account? <span>Login</span>
+        </p>
+
+        <div className='loginsignup-agree'>
+          <input type="checkbox" />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
     </div>
   )
 }
